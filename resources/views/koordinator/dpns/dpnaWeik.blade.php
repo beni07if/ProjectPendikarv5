2f1415d9d6c8520df4cbf233601f42fd4e098376
beni07if <beni07if@gmail.com>
2021-01-31 22:38:07 +0700

    test dpna weik

---
+++ resources/views/koordinator/dpns/dpnaWeik.blade.php
@@ -0,0 +1,67 @@
+@extends('layouts.masterKoordinator')
+
+@section('navbarTitle2')
+<a href="#" class="nav-link">DPNA</a>
+@endsection
+
+@section('breadcrumb')
+<a href="#" class="nav-link">Sistem Penilaian Pendikar / DPNA</a>
+@endsection
+
+@section('content')
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>DPNA</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="{{ route('mahasiswa.home') }}">Home</a></li>
+                        <li class="breadcrumb-item active">DPNA</li>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                    </div>
+                        <table id="example1" class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Nama</th>
+                                    <th>Keluarga</th>
+                                    <th>DPNA</th>
+                                    <th>Keterangan</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                               
+                            </tbody>
+                        </table>
+
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+            </div>
+            <!-- /.col -->
+        </div>
+        <!-- /.row -->
+    </section>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+@endsection
